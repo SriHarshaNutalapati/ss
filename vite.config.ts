be69354a9ac7686717494255bf9b4dd9fe1e8161
= <vnutalap@umd.edu>
2024-02-05 21:47:34 -0500

    first

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: { esbuildOptions: { target: "esnext" } }, // <-- Set this to resolve issue.
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  }
 });
-
-build: {
-  target: 'esnext'
-}
